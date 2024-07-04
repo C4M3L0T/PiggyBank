5ea9b80eb42e17200b333b4d1ceb1f48d18347fc
C4M3L0T <amrg1711@gmail.com>
2024-07-03 20:52:20 -0600

    Estructura del programa

--- alcancia.c
+++ alcancia.c
@@ -1,87 +1,190 @@
 #include <stdio.h>
-#include <string.h>
 #include <time.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-// Datos de usuario almacenados
-#define USERNAME "usuario"
-#define PASSWORD "pass"
-#define TOTAL_BALANCE 1000.0
+//Definimos la estructura de la transaccion
+typedef struct{
+    char *fecha;
+    char *hora;
+    char *tipo;
+    char *usuario;
+} Transaccion;
 
-// Función para obtener la fecha actual
-void getCurrentDate(int *day, int *month, int *year) {
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    *day = tm.tm_mday;
-    *month = tm.tm_mon + 1;
-    *year = tm.tm_year + 1900;
+// Definimos el nodo para la lista enlazada de transaccion
+typedef struct Nodo {
+    Transaccion transaccion;
+    struct Nodo *siguiente;
+} ListaTransaccion;
+
+// Definimos la estructura del usuario
+typedef struct{
+    char *usuario;
+    char *pass;
+    float saldo;
+    ListaTransaccion transacciones;
+    char *meta;
+} Usuario;
+
+// Funcion para inicializar la lista
+ListaTransaccion *Listatransaccion(ListaTransaccion *Lista){
+    Lista = NULL;
+    return Lista;
 }
 
-// Función para realizar el desglose de billetes
-void desgloseBilletes(int monto) {
-    int billetes[] = {500, 200, 100, 50, 20, 10, 5, 1};
-    int cantidadBilletes[sizeof(billetes) / sizeof(billetes[0])] = {0};
+// Funcion para agregar datos en la lista
+ListaTransaccion *AgregarTransaccion(ListaTransaccion *Lista,char *fecha,char *hora,char *tipo, char *usuario){
+    //Definimos la transaccion
+    Transaccion nuevaTransaccion;
+    nuevaTransaccion.fecha = fecha;
+    nuevaTransaccion.hora = hora;
+    nuevaTransaccion.tipo = tipo;
+    nuevaTransaccion.usuario = usuario;
 
-    for (int i = 0; i < sizeof(billetes) / sizeof(billetes[0]); i++) {
-        cantidadBilletes[i] = monto / billetes[i];
-        monto %= billetes[i];
-    }
-
-    printf("Desglose de billetes:\n");
-    for (int i = 0; i < sizeof(billetes) / sizeof(billetes[0]); i++) {
-        if (cantidadBilletes[i] > 0) {
-            printf("%d billete(s) de %d\n", cantidadBilletes[i], billetes[i]);
+    //Crear nodo y asignar transaccion
+    ListaTransaccion *nuevoNodo = (ListaTransaccion*)malloc(sizeof(ListaTransaccion));
+    nuevoNodo->transaccion = nuevaTransaccion;
+    nuevoNodo->siguiente = NULL;
+    
+    //Verificamos si es el primer nodo de la lista
+    if(Lista == NULL){
+        Lista = nuevoNodo;
+    }else{
+        // Encontrar el último nodo en la lista
+        ListaTransaccion *ultimoNodo = Lista;
+        while (ultimoNodo->siguiente != NULL) {
+            ultimoNodo = ultimoNodo->siguiente;
         }
+        // Agregar el nuevo nodo al final
+        ultimoNodo->siguiente = nuevoNodo;
     }
+    return Lista;
+}
+
+// Funcion para imprimir la lista
+void ImprimirListaTransaccion(ListaTransaccion *Lista){
+    while (Lista != NULL)
+    {
+        printf("Transaccion de %s\n",Lista->transaccion.tipo);
+        printf("Fecha: %s\n",Lista->transaccion.fecha);
+        printf("Hora: %s\n",Lista->transaccion.hora);
+        printf("La transaccion fue hecha por el usuario: %s\n",Lista->transaccion.usuario);
+        Lista = Lista->siguiente;
+    }
+}
+
+// Funciones para imprimir los menus
+
+void MenuInicio(){
+    printf("******** Bienvenid@ a la alcancia inteligente *************\n");
+    printf("a) Iniciar sesión\n");
+    printf("b) Salir\n");
+    printf("Elige una opcion:\n");
+}
+
+void MenuCRUD(Usuario usuario){
+    printf("************** Bienvenid@ de nuevo %s ***************\n",usuario.usuario);
+    printf("1) Ver saldo.\n");
+    printf("2) Retirar saldo.\n");
+    printf("3) Agregar saldo.\n");
+    printf("4) Ver historial.\n");
+    printf("5) Ver meta.\n");
+    printf("6) Cerrar sesion.\n");
+    printf("Seleccione una opcion:\n");
+}
+
+// Funcion Inicio de Sesion
+
+// Función para obtener la fecha actual
+void obtenerFechaHora(char *fecha, char *hora) {
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+
+    strftime(fecha, 20, "%Y-%m-%d", &tm);
+    strftime(hora, 10, "%H:%M:%S", &tm);
+}
+
+// Funcion para guardar Usuario
+
+void Guardar(Usuario usuario){
+    FILE *archivo = fopen("Usuarios.bin","w");
+    fwrite(&usuario, sizeof(Usuario),1, archivo);
+    fclose(archivo);
 }
 
 int main() {
-    char usuario[50], contrasena[50];
-    int day, month, year;
-    float maxRetiro, cantidadRetirar;
+    Usuario usuario;
+    char opc;
+    int opciones;
+    bool salir = 1;
+    char fecha[20];
+    char hora[10];
 
-    // Mostrar mensaje de bienvenida y presentación de uso
-    printf("Bienvenido al cajero automático.\n");
-    printf("Por favor, inicie sesión para continuar.\n");
+    // Definimos el usuario 
+    usuario.pass = "pass";
+    usuario.usuario = "Angel";
+    usuario.meta = "Silla de escritorio";
+    usuario.saldo = 0;
 
-    // Solicitar usuario y contraseña
-    printf("Usuario: ");
-    scanf("%s", usuario);
-    printf("Contraseña: ");
-    scanf("%s", contrasena);
+    // Definimos la Lista enlazada
+    ListaTransaccion *Lista = Listatransaccion(Lista);
 
-    // Comparar datos de inicio de sesión
-    if (strcmp(usuario, USERNAME) != 0 || strcmp(contrasena, PASSWORD) != 0) {
-        printf("Usuario o contraseña incorrectos.\n");
-        return 1;
+    //Lista = AgregarTransaccion(Lista,fecha,hora,"retiro",usuario.usuario);
+    while(1){
+        MenuInicio();
+        scanf("%c",&opc);
+
+        if(opc == 'a' || opc == 'A'){
+            //IniciarSesion();
+            do{          
+                MenuCRUD(usuario);
+                scanf("%i",&opciones);
+                switch(opciones){
+                    case 1:
+                        //VerSaldo();
+                    break;
+
+                    case 2:
+                        //Retirar;
+                        obtenerFechaHora(fecha,hora);
+                        Lista = AgregarTransaccion(Lista,fecha,hora,"Retiro",usuario.usuario);
+
+                    break;
+
+                    case 3:
+                        //Agregar;
+                        obtenerFechaHora(fecha,hora);
+                        Lista = AgregarTransaccion(Lista,fecha,hora,"Deposito",usuario.usuario);                        
+                    break;
+
+                    case 4:
+                        ImprimirListaTransaccion(Lista);
+                    break;
+
+                    case 5:
+                        //VerMeta();
+                    break;
+
+                    case 6:
+                        printf("Cerrando sesión...\n");
+                        salir = 0;
+                    break;
+
+                    default:
+                        printf("Opcion no valida\n");
+                    break;
+                    }
+                //Guardar(usuario);
+            }while(salir);
+            salir = 1;
+            
+        }else if(opc == 'b' || opc == 'B'){
+            printf("Hasta luego!\n");
+            break;
+        }else{
+            printf("Esa opcion no es correcta\n");
+        }
     }
-
-    // Obtener la fecha actual
-    //getCurrentDate(&day,&month,&year);
-    day = 4; //Ponemos esta fecha, ya que en el sistema de mi computadora estamos a ultimos de Junio C:
-
-
-    // Validar si el día es dentro de los primeros 5 días del mes
-    if (day > 5) {
-        printf("Solo se puede retirar en los primeros 5 días del mes.\n");
-        return 1;
-    }
-
-    // Calcular el monto máximo a retirar
-    maxRetiro = TOTAL_BALANCE * 0.40;
-    printf("El monto máximo que puede retirar es: %.2f\n", maxRetiro);
-
-    // Solicitar la cantidad a retirar
-    printf("Ingrese la cantidad a retirar: ");
-    scanf("%f", &cantidadRetirar);
-
-    // Validar la cantidad a retirar
-    if (cantidadRetirar > maxRetiro) {
-        printf("La cantidad no puede ser mayor al 40%% del total (%.2f).\n", maxRetiro);
-        return 1;
-    }
-
-    // Realizar el desglose de billetes
-    desgloseBilletes((int)cantidadRetirar);
-
+    
     return 0;
 }
